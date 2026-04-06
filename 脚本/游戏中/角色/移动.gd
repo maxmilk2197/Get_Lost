a14ba395f8d5aefa7a6381f2945be5c914fa8f09
maxmilk2197 <nahida1129@outlook.com>
2026-04-06 20:15:15 +0800

    发现并修改移动问题

--- 脚本/游戏中/角色/移动.gd
+++ 脚本/游戏中/角色/移动.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 
-@export var speed = 5.0
-@export var jump_velocity = 4.5
+@export var 速度 = 5.0
+@export var 跳跃速度 = 4.5
 
 
 func _physics_process(delta: float) -> void:
@@ -12,17 +12,17 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.y = 跳跃速度
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * 速度
+		velocity.z = direction.z * 速度
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, 速度)
+		velocity.z = move_toward(velocity.z, 0, 速度) 
 
 	move_and_slide()
